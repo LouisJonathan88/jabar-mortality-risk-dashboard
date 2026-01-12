2902c9c3a9f5f46c87992dee7ae0ebf7e4283651
LouisJonathan88 <jonathanlouis568@gmail.com>
2026-01-12 18:05:49 +0700

    first commit

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ risk, detail = load_data()
 # Header + Filter Tahun + Jenis Kematian
 # =============================
 st.markdown(
-    "<h1 style='text-align:center; margin-top: 10px;'>Peta Risiko Kematian dan Rekomendasi Penanganan Berbasis AI</h1>",
+    "<h1 style='text-align:center; margin-top: 10px;'>Peta Penyebaran Penyebab Kematian dan Rekomendasi Penanganan Berbasis AI</h1>",
     unsafe_allow_html=True
 )
 st.divider()
